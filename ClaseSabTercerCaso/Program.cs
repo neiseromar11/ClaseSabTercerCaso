a98f0665dfb5619839d7f6a41ce52fe9344b1793
Neiser omar <49031435+neiseromar11@users.noreply.github.com>
2023-09-01 22:05:05 -0500

    Agregar archivos de proyecto.

---
+++ ClaseSabTercerCaso/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese numero de alumnos : ");
+int NumAlumnos=int.Parse(Console.ReadLine());
+float PagoAlumno = 0;
+if (NumAlumnos >= 100)
+{
+    PagoAlumno = 65.0f;
+    
+}
+else {
+    if (NumAlumnos>=50) {
+        PagoAlumno = 70.0f;
+
+    }
+    else {
+        if (NumAlumnos > 30)
+        {
+
+            PagoAlumno = 95.0f;
+
+        }
+        else {
+
+            PagoAlumno = 4000 / NumAlumnos;
+        }
+    }
+  
+
+
+}
+float TOT = PagoAlumno * NumAlumnos;
+Console.WriteLine("El pago individual es " + PagoAlumno
+    + "\nEl pago total es " + TOT);
+
